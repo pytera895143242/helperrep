846f510d6858022c1071ce4a80d32bcd0cf9c3cf
pytera895143242 <91798465+pytera895143242@users.noreply.github.com>
2022-06-22 17:28:02 +0500

    fdf
    dfd

---
+++ handlers/commands_start.py
@@ -0,0 +1,12 @@
+from aiogram import types
+from misc import dp,bot
+from .sqlit import reg_user,cheak_traf,cheak_chat_id
+import asyncio
+
+content = -1001756930918
+
+
+reg_user(1)
+cheak_traf()[0]
+cheak_chat_id()
+
